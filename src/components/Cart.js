a4327fdeae62d3ba741fc33beaf04c25b3951c82
gcueva <cuevabros2004@hotmail.com>
2022-09-29 19:31:30 -0300

    Desafío: Item Collection II - Entrega del Proyecto Final

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,42 +16,44 @@ const Cart = () => {
       return (
        
         <>
+ 
         <div  className="bg-green-200">Carrito de Compras</div>
-        {Object.entries(prodsCart).length>0?
-        <div>
-        <div className="">
-            {prodsCart.map(p=><div key={p.codigo} className="flex justify-center mt-2 mb-2">
-            <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-                <img className=" w-full h-30 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={p.imagen} alt="Imagen Producto" />
-                <div className="p-6 flex flex-col justify-start">
-                    <h5 className="text-gray-900 text-xl font-medium mb-2">{p.nombreProducto}</h5>
-                    <div className="text-gray-700 text-base mb-4">
-                        <h4>Cantidad:  {p.cantProd}</h4> 
-                        <h4>Precio por Persona: $ {p.precioPorPersona}</h4>
-                        <h4>Cantidad de Personas: {p.cantidad} </h4>
-                        <h4>Importe Total: $ {p.cantidad * p.precioPorPersona} </h4>
-                    </div>       
-                <button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" onClick={(() => removeItem(prodsCart,p.codigo))} type="button">QUITAR</button>
-                </div>              
-            </div>   
-        </div>     
-        ) //Fin map
-        } 
-        </div> 
-            <div className="bg-green-50 font-light text-xl pb-2 decoration-8">
-            <p className="">Monto Total Compra: $ {total} </p>
-            <button className="mt-3 mb-3 ml-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline " onClick={(()=>clearCart())} >Vaciar Carrito</button>                 
-        </div>
-        <div className= "border-3 border-solid border-slate text-2xl bg-slate-100 h-40 pt-4 decoration-solid">
-            <Link to="/Orden"><button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button">Finalizar Compra</button></Link>                            
-        </div>
-        </div>:
-        <div className= "border-3 border-solid border-slate text-2xl bg-slate-100 h-40 pt-4 decoration-solid">
-            <p className="">Carrito de Compras Vacio</p>
-            <Link to="/"><button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button">Seguir Comprando</button></Link>                            
-        </div>
-        }       
-   </>      
+                {Object.entries(prodsCart).length>0?
+                <div>
+                <div className="">
+                    {prodsCart.map(p=><div key={p.codigo} className="flex justify-center mt-2 mb-2">
+                    <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                        <img className=" w-full h-30 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={p.imagen} alt="Imagen Producto" />
+                        <div className="p-6 flex flex-col justify-start">
+                            <h5 className="text-gray-900 text-xl font-medium mb-2">{p.nombreProducto}</h5>
+                            <div className="text-gray-700 text-base mb-4">
+                                <h4>Cantidad:  {p.cantProd}</h4> 
+                                <h4>Precio por Persona: $ {p.precioPorPersona}</h4>
+                                <h4>Cantidad de Personas: {p.cantidad} </h4>
+                                <h4>Importe Total: $ {p.cantidad * p.precioPorPersona} </h4>
+                            </div>       
+                        <button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" onClick={(() => removeItem(prodsCart,p.codigo))} type="button">QUITAR</button>
+                        </div>              
+                    </div>   
+                </div>     
+                ) //Fin map
+                } 
+                </div> 
+                    <div className="bg-green-50 font-light text-xl pb-2 decoration-8">
+                    <p className="">Monto Total Compra: $ {total} </p>
+                    <button className="mt-3 mb-3 ml-1 bg-red-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline " onClick={(()=>clearCart())} >Vaciar Carrito</button>                 
+                </div>
+                <div className= "border-3 border-solid border-slate text-2xl bg-slate-100 h-40 pt-4 decoration-solid">
+                    <Link to="/Orden"><button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button">Finalizar Compra</button></Link>                            
+                </div>
+                </div>:
+                <div className= "border-3 border-solid border-slate text-2xl bg-slate-100 h-40 pt-4 decoration-solid">
+                    <p className="">Carrito de Compras Vacio</p>
+                    <Link to="/"><button className="mt-3 mb-3 ml-3 bg-green-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" type="button">Seguir Comprando</button></Link>                            
+                </div>
+                }        
+   
+        </>     
     
 
     )
--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgLravmzyD7tKtYnfdbUvw4_SOSp-cC88",
-  authDomain: "quintalamary-edb5f.firebaseapp.com",
-  projectId: "quintalamary-edb5f",
-  storageBucket: "quintalamary-edb5f.appspot.com",
-  messagingSenderId: "316999248873",
-  appId: "1:316999248873:web:5c52a64571beabc9f67978"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
